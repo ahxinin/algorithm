7efa6acc6c732a71042383b517451cdf1336ebaf
阿昕 <ahxin1989@gmail.com>
2023-07-04 13:45:52 +0800

    基于数组或链表实现栈

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # algorithm
 ### 代码结构
+- 数据结构 datastructure
 - 空间复杂度 spacecomplexity
 - 时间复杂度 timecomplexity
---
+++ src/main/java/com/ahxinin/datastructure/ListNode.java
@@ -0,0 +1,17 @@
+package com.ahxinin.datastructure;
+
+/**
+ * 链表节点类
+ */
+public class ListNode {
+    //节点值
+    int val;
+
+    //指向下一个节点的指针
+    ListNode next;
+
+    //构造函数
+    ListNode(int x){
+        val = x;
+    }
+}
